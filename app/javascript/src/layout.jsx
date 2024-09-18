3903e79cd482f08979df930a3d78066b18796f77
Devon <devonhamill93@gmail.com>
2024-09-18 15:33:10 -0400

    Bootstrap and basic app structure

---
+++ app/javascript/src/layout.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Layout = (props) => {
+    return (
+        <React.Fragment>
+            <nav className="navbar navbar-expand navbar-light bg-light">
+                <div className="container">
+                    <a className="navbar-brand text-primary" href="/">Home</a>
+                </div>
+            </nav>
+        </React.Fragment>
+    );
+}
+
+export default Layout;
