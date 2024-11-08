bb0183414a44b7f1f307312eff8c37baded040f8
Devon <devonhamill93@gmail.com>
2024-11-08 15:13:15 -0500

    Updated production.js for minification config

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,26 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'production'
+process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 
-const environment = require('./environment')
+const environment = require('./environment');
+const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = environment.toWebpackConfig()
+// Customize the optimization settings
+environment.config.optimization = {
+  minimize: true, // Set to `false` if you want to disable minification
+  minimizer: [
+    new TerserPlugin({
+      terserOptions: {
+        compress: {
+          drop_console: false, // Keep console logs for debugging
+          drop_debugger: false, // Keep debugger statements
+        },
+        mangle: false, // Disable mangling of variable names for easier debugging
+        format: {
+          comments: false, // Remove comments
+        },
+      },
+      extractComments: false, // Do not extract comments to a separate file
+    }),
+  ],
+};
+
+module.exports = environment.toWebpackConfig();
