ac5b60583034ba81e7bd6fd974c75446a418f117
Devon <devonhamill93@gmail.com>
2025-01-05 14:48:05 -0500

    Updated to 2025 members application

--- app/javascript/src/members/membersApplication.jsx
+++ app/javascript/src/members/membersApplication.jsx
@@ -13,9 +13,10 @@ class MembersApplication extends React.Component {
         return (
             <div className="container text-center">
                 <h2 className="mt-5">Member Application</h2>
-                <h4 className="mt-3 mb-3">To become a member, print and complete the application below.</h4>
-                <embed id="application-pdf" src="/members/LOHRC 2022 membership application.pdf" type="application/pdf"></embed>
-                <h4 className="mt-3 mb-5">To submit your application, email it to robertmonico@trentu.ca</h4>
+                <h4 className="mt-3 mb-3">To become a member, print and complete the application below</h4>
+                <h4 className="mt-3 mb-3">Or complete the online form <a href="https://docs.google.com/forms/d/e/1FAIpQLScLi3oP_UmNS-IHrUYYTqt0VtTa-LFyZXEf-sziqeAu-audAA/viewform" target="blank">here</a></h4>
+                <embed id="application-pdf" src="/members/2025 MEMBERSHIP APP.pdf" type="application/pdf"></embed>
+                <h4 className="mt-3 mb-5">To submit your application, email it to lakeontariohrc@outlook.com</h4>
             </div>
         )
     }
